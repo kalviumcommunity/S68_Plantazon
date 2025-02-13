155189eeeec0bc34bcb82a35831b64ba0b8c73b6
irinesara <irine.ixa@gmail.com>
2025-02-13 13:52:50 +0530

    updated

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express');
-const { connectDB } = require('./db');
+const { connectDB } = require('./db');  
+const bodyParser = require('body-parser');
+const routes = require('./routes'); 
 
 const app = express();
 
 require('dotenv').config();
-const port = process.env.PORT || 8080;
-const url = process.env.db_url;
+const port = process.env.PORT || 5000;  
+const url = process.env.db_url; 
 
 
-app.listen(port, async() => {
+app.use(bodyParser.json());
 
-  try{
-    await connectDB(url);
-    console.log(`Server is running on port ${port}`);
-  }
-  catch(error){
-    console.error(error);
+
+app.listen(port, async () => {
+  try {
+    await connectDB(url);  
+    console.log(`Connected to the database and server is running on port ${port}`);
+  } catch (error) {
+    console.error('Database connection failed:', error);
   }
 });
 
+// Define CRUD routes
+app.use('/api', routes);  // Use the routes defined in routes.js (prefixed with '/api')
 
-app.get('/', (req, res) => {    
-    res.send('Hello World!');
+// Root endpoint (just a simple test endpoint)
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
