3ac62392bd0093af627380738c0c6842136fe6ce
irinesara <irine.ixa@gmail.com>
2025-03-01 10:43:08 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Plantcomponent from "./components/Plantcomponent";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="/plants" element={<Plantcomponent />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
